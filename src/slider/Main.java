42dbd06525223c4b85131ac052dad5b6a8a0fbcc
adieb <mrizqiamiruladieb@gmail.com>
2025-03-16 15:54:27 +0700

    First commit!

---
+++ src/slider/Main.java
@@ -0,0 +1,11 @@
+package slider;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        // JSlider = GUI component that lets user enter a value
+        //           by using an adjustable sliding knob on a track
+    }
+    
+}
